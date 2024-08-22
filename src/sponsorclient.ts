68c58d1fb849abbb7383673afa65ae6b97f67ba2
reneecok <renee.zpro@gmail.com>
2024-08-22 17:58:49 +0800

    feat: client sdk for eoa-paymaster

---
+++ src/sponsorclient.ts
@@ -0,0 +1,43 @@
+import {ethers} from 'ethers'
+
+export enum WhiteListType {
+  FromAccountWhitelist = 'FromAccountWhitelist',
+  ToAccountWhitelist = 'ToAccountWhitelist',
+  ContractMethodSigWhitelist = 'ContractMethodSigWhitelist',
+  BEP20ReceiverWhiteList = 'BEP20ReceiverWhiteList'
+}
+
+export type WhiteListArgs = {
+  PolicyUUID: string
+  WhiteListType: WhiteListType
+  Values: string[]
+}
+
+export type GetWhiteListArgs = {
+  PolicyUUID: string
+  WhiteListType: WhiteListType
+  Offset: number
+  Limit: number
+}
+
+export class SponsorClient extends ethers.JsonRpcProvider {
+  constructor(url:string) {
+    super(url)
+  }
+
+  async addToWhitelist(params: WhiteListArgs): Promise<boolean> {
+    return this.send('pm_addToWhitelist', [params])
+  }
+
+  async removeFromWhitelist(params: WhiteListArgs): Promise<boolean> {
+    return this.send('pm_rmFromWhitelist', [params])
+  }
+
+  async emptyWhitelist(params: WhiteListArgs): Promise<boolean> {
+    return this.send('pm_emptyWhitelist', [params])
+  }
+
+  async getWhitelist(params: GetWhiteListArgs): Promise<string[]> {
+    return this.send('pm_getWhitelist', [params])
+  }
+}
