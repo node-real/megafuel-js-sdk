920dfb5ca67140106f290d4ef695b053227a26a2
reneecok <renee.zpro@gmail.com>
2024-08-23 21:25:25 +0800

    feat: add 3 validatos

---
+++ dist/tsc/sponsorclient.js
@@ -0,0 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SponsorClient = exports.WhiteListType = void 0;
+const tslib_1 = require("tslib");
+const ethers_1 = require("ethers");
+var WhiteListType;
+(function (WhiteListType) {
+    WhiteListType["FromAccountWhitelist"] = "FromAccountWhitelist";
+    WhiteListType["ToAccountWhitelist"] = "ToAccountWhitelist";
+    WhiteListType["ContractMethodSigWhitelist"] = "ContractMethodSigWhitelist";
+    WhiteListType["BEP20ReceiverWhiteList"] = "BEP20ReceiverWhiteList";
+})(WhiteListType = exports.WhiteListType || (exports.WhiteListType = {}));
+class SponsorClient extends ethers_1.ethers.JsonRpcProvider {
+    constructor(url, network, options) {
+        super(url, network, options);
+    }
+    addToWhitelist(params) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return this.send('pm_addToWhitelist', [params]);
+        });
+    }
+    removeFromWhitelist(params) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return this.send('pm_rmFromWhitelist', [params]);
+        });
+    }
+    emptyWhitelist(params) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return this.send('pm_emptyWhitelist', [params]);
+        });
+    }
+    getWhitelist(params) {
+        return tslib_1.__awaiter(this, void 0, void 0, function* () {
+            return this.send('pm_getWhitelist', [params]);
+        });
+    }
+}
+exports.SponsorClient = SponsorClient;
+//# sourceMappingURL=sponsorclient.js.map
